f36a35977eb42c00ee771e7c62db69f11607e311
Henrik Schopmans <h.schopmans@gmail.com>
2015-03-11 20:54:22 +0000

    Implemented dmenu starting

--- i3-starter.py
+++ i3-starter.py
@@ -11,3 +11,27 @@
     # when the window isn't on saved workspace, then move it there
 
     # repeat for x seconds every y seconds
+
+import subprocess
+
+
+def run_dmenu():
+    path_process = subprocess.Popen(['dmenu_path'], stdout=subprocess.PIPE)
+    dmenu_process = subprocess.Popen(['dmenu'], stdin=path_process.stdout, stdout=subprocess.PIPE)
+
+    path_process.wait()
+    dmenu_process.wait()
+
+    return dmenu_process.communicate()[0].decode("utf-8")
+
+def active_workspace():
+    pass
+
+
+application = run_dmenu()
+
+current_workspace = subprocess.check_output('i3-msg', '-t', 'get_workspaces');
+
+
+
+#dmenu_path | dmenu "$@"
